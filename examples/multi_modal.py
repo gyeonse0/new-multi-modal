2f0572d8ce39b4f696267abe17b6e53d309e57bb
iamjunyeong <148768652+iamjunyeong@users.noreply.github.com>
2024-03-27 01:25:50 +0900

    Update multi_modal.py

--- examples/multi_modal.py
+++ examples/multi_modal.py
@@ -15,8 +15,8 @@ from MultiModalState import *
 SEED = 1234
 rnd_state = np.random.RandomState(None)
 
-vrp_file_path = r'C:\Users\User\OneDrive\바탕 화면\examples\data\multi_modal_data.vrp'
-sol_file_path = r'C:\Users\User\OneDrive\바탕 화면\examples\data\multi_modal_data.sol'
+vrp_file_path = r'C:\Users\User\OneDrive\multi-modal\new-multi-modal-main\examples\data\multi_modal_data.vrp'
+sol_file_path = r'C:\Users\User\OneDrive\multi-modal\new-multi-modal-main\examples\data\multi_modal_data.sol'
 
 file_reader = FileReader()
 data = file_reader.read_vrp_file(vrp_file_path)
@@ -41,7 +41,7 @@ destroy_probabilities = [0.33, 0.34, 0.33]  # 각각의 파괴 연산자에 대�
 repair_probabilities = [0.6, 0.1, 0.3]  # 각각의 수리 연산자에 대한 확률
 
 init = initial_truck
-runtime_seconds = 1800
+runtime_seconds = 5
 
 # 초기 설정
 start_temperature = 100
@@ -130,3 +130,100 @@ for name, count in destroy_counts.items():
 print("\nRepair Operator Counts(#):")
 for name, count in repair_counts.items():
     print(f"{name}: {count}")
+
+#routes_t, routes_soc = MultiModalState(current_states[min_index]).objective_value_list() #로 route들에 대한 OFV 리스트 n개 생성 input으로 state: MultiModalState(current_states[min_index])
+total_route, routes_soc_truck = MultiModalState(current_states[min_index]).truck_soc()
+total_route, routes_soc_drone = MultiModalState(current_states[min_index]).drone_soc()
+
+# for i, route in enumerate(total_route):
+#     plt.figure(figsize=(8, 6))
+    
+#     # Plot truck data
+#     plt.plot(range(len(route)), routes_soc_truck[i], marker='o', linestyle='-', label='Truck')
+    
+#     # Plot drone data
+#     plt.plot(range(len(route)), routes_soc_drone[i], marker='o', linestyle='-', label='Drone')
+    
+#     plt.title(f"Route {i+1} OFV Graph")
+#     plt.xlabel("Customer")
+#     plt.ylabel("SOC (%)")
+#     plt.grid(True)
+#     plt.legend()  # Add legend to differentiate between truck and drone
+    
+#     # Set x ticks to show all customers
+#     plt.xticks(range(len(route)), route)
+    
+#     plt.show()
+
+total_routes = MultiModalState(current_states[min_index]).routes.routes
+for i, route in enumerate(total_routes):
+    plt.figure(figsize=(8, 6))
+    truck_path = [x if route[idx][1] != ONLY_DRONE else None for idx, x in enumerate(routes_soc_truck[i])]
+    # None 값을 이전 값과 다음 값의 중간 값으로 대체
+    for j in range(1, len(truck_path) - 1):
+        if truck_path[j] is None:
+            left_index = j - 1
+            right_index = j + 1
+            left_value = None
+            right_value = None
+            
+            # 이전 값이 None이면서 인덱스가 리스트 범위를 벗어나지 않을 때까지 이동
+            while left_index >= 0 and truck_path[left_index] is None:
+                left_index -= 1
+            if left_index >= 0:
+                left_value = truck_path[left_index]
+            
+            # 다음 값이 None이면서 인덱스가 리스트 범위를 벗어나지 않을 때까지 이동
+            while right_index < len(truck_path) and truck_path[right_index] is None:
+                right_index += 1
+            if right_index < len(truck_path):
+                right_value = truck_path[right_index]
+            
+            # 이전 값과 다음 값이 모두 None이 아닌 경우에만 중간 값으로 대체
+            if left_value is not None and right_value is not None:
+                truck_path[j] = (left_value + right_value) / 2
+    # Plot truck data
+    plt.plot(range(len(route)), truck_path , marker='o', linestyle='-', label='Truck')
+    
+    drone_path = [x if route[idx][1] != ONLY_TRUCK else None for idx, x in enumerate(routes_soc_drone[i])]
+    # None 값을 이전 값과 다음 값의 중간 값으로 대체
+    for j in range(1, len(drone_path) - 1):
+        if drone_path[j] is None:
+            left_index = j - 1
+            right_index = j + 1
+            left_value = None
+            right_value = None
+            
+            # 이전 값이 None이면서 인덱스가 리스트 범위를 벗어나지 않을 때까지 이동
+            while left_index >= 0 and drone_path[left_index] is None:
+                left_index -= 1
+            if left_index >= 0:
+                left_value = drone_path[left_index]
+            
+            # 다음 값이 None이면서 인덱스가 리스트 범위를 벗어나지 않을 때까지 이동
+            while right_index < len(drone_path) and drone_path[right_index] is None:
+                right_index += 1
+            if right_index < len(drone_path):
+                right_value = drone_path[right_index]
+            
+            # 이전 값과 다음 값이 모두 None이 아닌 경우에만 중간 값으로 대체
+            if left_value is not None and right_value is not None:
+                drone_path[j] = (left_value + right_value) / 2
+    # Plot drone data
+    plt.plot(range(len(route)), drone_path, marker='o', linestyle='-', label='Drone')
+    
+    plt.title(f"Route {i+1} OFV Graph")
+    plt.xlabel("Customer")
+    plt.ylabel("SOC (%)")
+    plt.grid(True)
+    plt.legend()  # Add legend to differentiate between truck and drone
+    """
+    none_indices_t = [i for i, value in enumerate(truck_path) if value is None]
+    none_indices_d = [i for i, value in enumerate(drone_path) if value is None]
+    plt.scatter(none_indices_t, [0] * len(none_indices_t), marker='x', color='red', label='None')
+    plt.scatter(none_indices_d, [0] * len(none_indices_d), marker='x', color='blue', label='None')
+    """
+    # Set x ticks to show all customers
+    plt.xticks(range(len(route)), [customer[0] for customer in route])
+    
+    plt.show()
